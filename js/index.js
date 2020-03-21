a6f36b186a487a420019f20ef0460e5dd65eed42
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-03-21 15:28:32 +0900

    refactored fuc structure

--- js/index.js
+++ js/index.js
@@ -1,99 +1,76 @@
 window.onload = function(){
-  async function loadData() {
-    // Import data from json file
-    console.log('Importing data')
-    const response = await fetch('data/data.json');
-    const data = await response.json();
-    console.log(data);
-    
-    // Separate data into categories
-    console.log('Separating data')
-    const types = data['incomes']['rows']
-    const dates = data['incomes']['columns']
-    const incomes = data['incomes']['values']
-    console.log(dates, types, incomes)
 
-    console.log('Num of types', types.length)
-    const len = types.length
-    console.log('Num of incomes', incomes.length)
-    
-    // return len, types, dates, incomes
-    // await drawIncomeChart(len, types, dates, incomes)
-    let config = {
-      type: 'bar',
-      data: {
-        labels: dates,
-        datasets: []
-      },
-      options: {
-        scales: {
-          xAxes: [{stacked: true}],
-          yAxes: [{stacked: true}]
-        }
+  $.get('../data/data.json').then(function(data) {
+    console.log(data)
+    // loadData(data)
+    new Chart(document.getElementById('canvas').getContext('2d'), loadData(data))
+  })
+  // this.loadData()
+}
+
+function getPaycheckData() {}
+
+// async function loadData() {
+function loadData(data) {
+  
+  // # Define struct ----------------------------------
+  let config = {
+    type: 'bar',
+    data: {
+      labels: [],
+      datasets: []
+    },
+    options: {
+      scales: {
+        xAxes: [{stacked: true}],
+        yAxes: [{stacked: true}]
       }
     }
-
-    // Setup
-    console.log('Setup config for datasets with struct')
-    let struct = {
-      label: [],
-      data: [],
-      backgroundColor: [],
-      // borderColor: 'rgba(54, 162, 235, 1)',
-      borderWidth: 1
-    }
-
-    const cloning = function() {
-      for (let index = 0; index < len; index++) {
-        // Loop for the length of rows(types)
-        let deepClone = JSON.parse(JSON.stringify(struct))
-        config.data.datasets.push(deepClone)
-        console.log('struct:', struct)
-        console.log('deepClone:', deepClone)
-        console.log('Settup...')
-      }
-    }
-
-    await cloning()
-    console.log('datasets:', config.data.datasets)
-
-    // Parse
-    console.log('Insert data')
-    types.forEach(function(type, i) {
-      console.log(type)
-      console.log(window.chartColors)
-      config.data.datasets[i].label.push(type)
-    })
-    incomes.forEach(function(income, i) {
-      console.log(income)
-      let arr = income
-      let iterator = arr.values()
-      for(let value of iterator) {
-        console.log(value)
-        config.data.datasets[i].data.push(value)
-      }
-    })
-
-    let colors = []
-    Object.keys(window.chartColors).forEach(function(key, i) {
-      if (i < len) colors.push(key)
-    })
-    console.log('color palette:', colors)
-    colors.forEach(function(color, i) {
-      console.log('color hex:', window.chartColors[color])
-      config.data.datasets[i].backgroundColor = window.chartColors[color]
-    })
-
-    // let ctx = document.getElementById('canvas').getContext('2d');
-    // window.myChart = new Chart(ctx, config)
-    window.myChart = new Chart(document.getElementById('canvas').getContext('2d'), config)
-    // console.log('config(in):', config)
-    // return config
   }
 
-  loadData();
-  // new Chart(document.getElementById('canvas').getContext('2d'), loadData)
-  // let config = loadData();
-  // console.log('config:', config)
-  // new Chart(document.getElementById('canvas').getContext('2d'), config)
-};
+  let dataset = {
+    label: [],
+    data: [],
+    backgroundColor: [],
+    // borderColor: 'rgba(54, 162, 235, 1)',
+    borderWidth: 1
+  }// END OF Define struct----------------------------------
+
+  // # Preprocess data ----------------------------------
+  const types = data['incomes']['rows']
+  const dates = data['incomes']['columns']
+  const incomes = data['incomes']['values']
+  const len = types.length
+  // END OF Preprocess data ----------------------------------
+
+  // # Build config structure ----------------------------------
+  // Prepare dataset structure
+  for (let index = 0; index < len; index++) {
+    // Loop for the length of rows(types)
+    let deepClone = JSON.parse(JSON.stringify(dataset))
+    config.data.datasets.push(deepClone)
+  }// ENDO OF Build config structure ----------------------------------
+  
+  // Insert data to structure ----------------------------------
+  config.data.labels.push(...dates)
+  types.forEach(function(type, i) {
+    config.data.datasets[i].label.push(type)
+  })
+  incomes.forEach(function(income, i) {
+    let arr = income
+    let iterator = arr.values()
+    for(let value of iterator) {
+      config.data.datasets[i].data.push(value)
+    }
+  })
+
+  let colors = []
+  Object.keys(window.chartColors).forEach(function(key, i) {
+    if (i < len) colors.push(key)
+  })
+  colors.forEach(function(color, i) {
+    config.data.datasets[i].backgroundColor = window.chartColors[color]
+  })// END OF Insert data to structure ----------------------------------
+
+  return config
+}
